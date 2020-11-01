d46eb0f865defb50cdedca5ee6c064ad2902f32a
food91 <saberxk@163.com>
2020-11-01 17:01:19 +0800

    完善天气界面布局

--- app/src/main/java/acitivity/DetailWeatherActivity.java
+++ app/src/main/java/acitivity/DetailWeatherActivity.java
@@ -1,47 +1,22 @@
 package acitivity;
 
 
-import android.app.Activity;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.ScrollView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.Description;
-import com.github.mikephil.charting.components.MarkerView;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
-import com.google.android.material.appbar.AppBarLayout;
-import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.jaeger.library.StatusBarUtil;
 import com.orhanobut.logger.Logger;
-import com.qmuiteam.qmui.widget.QMUIFontFitTextView;
 import com.xiekun.myapplication.R;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import Entity.WeatherData;
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import util.UtilX;
-import control.WeatherControl;
-import view.DetailsMarkerView;
 import view.TextViewRidus;
 import view.WeatherDetailsView;
 
@@ -59,8 +34,32 @@ public class DetailWeatherActivity extends AppCompatActivity {
 
 
     @BindView(R.id.weather_toolbar)
-     Toolbar mToolbar;
+    Toolbar mToolbar;
     WeatherData weatherData;
+    @BindView(R.id.weatherd_view_wdv)
+    WeatherDetailsView weatherdViewWdv;
+    @BindView(R.id.md_wap)
+    TextViewRidus mdWap;
+    @BindView(R.id.tv_thisday)
+    TextView tvThisday;
+    @BindView(R.id.tv_thisday_ce)
+    TextView tvThisdayCe;
+    @BindView(R.id.tv_tomoroday)
+    TextView tvTomoroday;
+    @BindView(R.id.tv_tomoroday_ce)
+    TextView tvTomorodayCe;
+    @BindView(R.id.tv_afterto)
+    TextView tvAfterto;
+    @BindView(R.id.tv_afterto_ce)
+    TextView tvAftertoCe;
+    @BindView(R.id.view_need_offset)
+    CoordinatorLayout viewNeedOffset;
+    @BindView(R.id.iv_thisday_wea)
+    ImageView ivThisdayWea;
+    @BindView(R.id.iv_today_wea)
+    ImageView ivTodayWea;
+    @BindView(R.id.iv_aftertoday_wea)
+    ImageView ivAftertodayWea;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -79,26 +78,26 @@ public class DetailWeatherActivity extends AppCompatActivity {
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
-        mViewNeedOffset=findViewById(R.id.view_need_offset);
-        mTextViewRidus=findViewById(R.id.md_wap);
-        StatusBarUtil.setTranslucentForImageView(this,0,mViewNeedOffset);
+        mViewNeedOffset = findViewById(R.id.view_need_offset);
+        mTextViewRidus = findViewById(R.id.md_wap);
+        StatusBarUtil.setTranslucentForImageView(this, 0, mViewNeedOffset);
     }
 
     protected void init() {
 
         getData();
         setSupportActionBar(mToolbar);
-        weatherDetailsView=findViewById(R.id.weatherd_view_wdv);
+        weatherDetailsView = findViewById(R.id.weatherd_view_wdv);
         setviewdata(weatherData);
 
     }
 
-    private void setviewdata(WeatherData weatherData){
-        if(weatherData!=null&&weatherDetailsView!=null)
+    private void setviewdata(WeatherData weatherData) {
+        if (weatherData != null && weatherDetailsView != null)
             weatherDetailsView.getData(weatherData);
     }
 
-    private void setToolbar(){
+    private void setToolbar() {
         mToolbar.setTitle(weatherData.getCity());
         mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
@@ -108,13 +107,20 @@ public class DetailWeatherActivity extends AppCompatActivity {
         });
     }
 
-    private void getData(){
+    private void getData() {
         Logger.d("getData");
-        weatherData= (WeatherData) getIntent().getSerializableExtra(WeatherData.DATANAME);
-        if(weatherData!=null){
+        weatherData = (WeatherData) getIntent().getSerializableExtra(WeatherData.DATANAME);
+        if (weatherData != null) {
             mTextViewRidus.setText(weatherData.getData().get(0).getAir_tips());
+            tvThisday.setText("今天·" + weatherData.getData().get(0).getWea());
+            tvThisdayCe.setText(weatherData.getData().get(0).getTem());
+            tvTomoroday.setText("明天·" + weatherData.getData().get(1).getWea());
+            tvTomorodayCe.setText(weatherData.getData().get(1).getTem());
+            tvTomoroday.setText("后天·" + weatherData.getData().get(2).getWea());
+            tvAftertoCe.setText(weatherData.getData().get(2).getTem());
+
             Logger.d(weatherData.toString());
-        }else{
+        } else {
             Logger.d("weaterData is  null");
         }
 
