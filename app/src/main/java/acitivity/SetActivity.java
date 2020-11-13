9ed27103f3feb3b9649a178581c414abcfd86004
food91 <saberxk@163.com>
2020-11-13 18:19:56 +0800

    增加了设置界面

--- app/src/main/java/acitivity/SetActivity.java
+++ app/src/main/java/acitivity/SetActivity.java
@@ -4,18 +4,27 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 
+import com.jaeger.library.StatusBarUtil;
 import com.xiekun.myapplication.R;
 
 public class SetActivity extends Xactivity {
+
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set);
+        init();
     }
 
     @Override
     protected void init() {
+
+
+        
+        StatusBarUtil.setColor(this,0,0);
+
 
     }
 }
